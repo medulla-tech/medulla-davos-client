c8a8da90671ffcb5794cff6951ad998f2fa1141f
Nicolas Lécureuil <neoclust@mageia.org>
2023-10-05 16:37:58 +0200

    (python3 port) Fix encoding

--- squashfs_override/usr/lib/python3/dist-packages/davos/inventory.py
+++ squashfs_override/usr/lib/python3/dist-packages/davos/inventory.py
@@ -47,10 +47,10 @@ class Inventory(object):
             stats[nicname] = pnic[nicname].bytes_sent
         self.interface = max(stats, key=stats.get)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        info = fcntl.ioctl(sock.fileno(), 0x8927,  struct.pack('256s', self.interface[:15]))
+        info = fcntl.ioctl(sock.fileno(), 0x8927,  struct.pack('256s', self.interface[:15].encode('utf-8')))
         self.macaddress = ':'.join(['%02x' % ord(char) for char in info[18:24]])
-        self.ipaddress = socket.inet_ntoa(fcntl.ioctl(sock.fileno(), 0x8915, struct.pack('256s', self.interface[:15]))[20:24])
-        self.netmask = socket.inet_ntoa(fcntl.ioctl(sock.fileno(), 0x891b, struct.pack('256s', self.interface))[20:24])
+        self.ipaddress = socket.inet_ntoa(fcntl.ioctl(sock.fileno(), 0x8915, struct.pack('256s', self.interface[:15].encode('utf-8')))[20:24])
+        self.netmask = socket.inet_ntoa(fcntl.ioctl(sock.fileno(), 0x891b, struct.pack('256s', self.interface.encode('utf-8')))[20:24])
 
         # Add the connected network interface
         self.addNetwork()
