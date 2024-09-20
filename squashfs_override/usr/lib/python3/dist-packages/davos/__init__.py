33021dcbae3085a5b1e3f6f09ff0f38cbcfbe5da
Nicolas Lécureuil <neoclust@mageia.org>
2024-09-20 10:18:45 +0200

    Use raw strings for the allowed variable

--- squashfs_override/usr/lib/python3/dist-packages/davos/__init__.py
+++ squashfs_override/usr/lib/python3/dist-packages/davos/__init__.py
@@ -253,7 +253,7 @@ class davosManager(object):
             return False
         if hostname[-1] == ".":
             hostname = hostname[:-1] # strip exactly one dot from the right, if present
-        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        allowed = re.compile(r"(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
         return all(allowed.match(x) for x in hostname.split("."))
 
     def confirm(self,response):
