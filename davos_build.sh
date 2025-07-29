c65805bbbfcb4fc9db8d93cf14523be28346021f
SPO <spointu@medulla-tech.io>
2025-07-29 07:45:48 +0200

    (clonezilla) Use stable version of Clonezilla

--- davos_build.sh
+++ davos_build.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Edit these line to update clonezilla version
-version="20250404-oracular"
+version="20250620-plucky"
 arch="amd64"
-base_url="http://free.nchc.org.tw/clonezilla-live/alternative/testing/$version/"
+base_url="http://free.nchc.org.tw/clonezilla-live/alternative/stable/"
 
 # =============================================================
 # Don't edit anything below these lines 
